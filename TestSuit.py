b4f89d00a44e3470bb234b6f40adb3d01de6941f
thomassweetnam <ec221018@qmul.ac.uk>
2023-10-19 12:01:14 +0100

    h

--- TestSuit.py
+++ TestSuit.py
@@ -1,6 +1,21 @@
-import unittest
 import requirements
-import wk101
+from wk101 import HelloWorld
+import unittest
 
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 if __name__ == '__main__':
-    requirements.
+    unittest.main()
